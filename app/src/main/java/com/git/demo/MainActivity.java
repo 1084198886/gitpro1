92d84befd5dc299ad34abb02eb4ebde5f9f7521d
gengqiyun <gengqiyun@newcapec.net>
2018-12-26 18:26:29 +0800

    release提交

--- app/src/main/java/com/git/demo/MainActivity.java
+++ app/src/main/java/com/git/demo/MainActivity.java
@@ -9,15 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //   fff
-        // hotfix1.0.0修改；
-        // feature 1.0.1修改
-        //  修改feature  1.0.1分支
-        // develop
-        // rele22222222222222222；feature/1.0.3修改；
-
-        // feature/1.0.2修改；
-        // hotfix 1.0.5 xiugai
     }
 }
